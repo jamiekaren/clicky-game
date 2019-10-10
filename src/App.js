31890f1f432de4c301ea08f96993209e8b1d1376
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-10-10 17:56:11 -0400

    REACT

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import React from "react";
 import ImageCard from "./components/ImageCard";
 
-const images = [
-  {
-    id: 1,
-    src: "src/components/images/bird.jpg"
-  },
 
-  {
-    id: 2,
-    src: "src/components/images/bunny.jpg"
-  },
-  {
-    id: 3,
-  
-    src: "src/components/images/dog.jpg"
-  },
-  {
-    id: 4,
-    src: "src/components/images/fox.jpg"
-  },
-  {
-    id: 5,
-    src: "src/components/images/hedgehog.jpg"
-  },
-  {
-    id: 6,
-    src: "src/components/images/horse.jpg"
-  }
-];
+
 
 function App() {
-  return <ImageCard images={images} />;
+  return (
+   
+
+  )
 }
 
 export default App;
--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-// Whenever we try to render an array containing JSX, React knows to render each JSX element separately
-function ImageCard(props) {
-  // Using the filter method, we can create a new array containing only groceries which haven't been purchased
-   return (
+class ImageCard extends Component {
+  // setting initial state
+  state = {
+    imageArray: ["bird", "bunny", "dog", "fox", "hedgehog"],
+    userChosen: ""
+  };
+
+createImage = () => {
+
+imageArray.map(item => (
+  <img alt="" src={item}/>
+
+))
+
+
+}
+
+
+render() {
+  return (
     <div>
-      {props.images.map(item => (
-        <img alt={props.id} src={item.src}/>     
-      ))}
+    
+
+
     </div>
+
   );
+}
+
+
 }
 
 export default ImageCard;
