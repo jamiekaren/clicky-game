fe91e607658651979b017783ab91b86277329b13
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-10-11 21:39:27 -0400

    Tried to add a function to be called when component mounts to go through images in json, and then set it to a state called images. This way I could create a new array later when the user clicks and set that to state, but it's not right.

--- src/components/CardContainer.js/index.js
+++ src/components/CardContainer.js/index.js
@@ -11,22 +11,33 @@ class CardContainer extends Component {
     highScore: 0
   };
 
-  // componentDidMount() {
-  //   this.createCards();
-  // }
 
-  loopImages = () => {
-    imagesData.filter(image => 
-      this.setState({imagesArray: image} )
-      )
+//When component mounts, run the loop images function
+  componentDidMount() {
+    this.loopImages();
   }
 
 
+//Loop through JSON data here and create a new array to set the state of **this** component?
+//But we def can't do that, because we'd be looping every time, we only want to get JSON once?
+
+
+  loopImages = () => {
+    imagesData.filter(image =>
+      this.setState({
+        imagesArray: image
+      })
+    );
+  }
+
+  // I know that I can loop through my Card here to get my images if I pull from my JSON file
+  // But I want to render it from STATE, not JSON... how??
   render() {
     return (
       <Wrapper>
         <h1>Clicky Game!</h1>
         {imagesData.map(image =>
+          //Runs my CreateCard "stupid" function, which is pulling in my JSON data to create my images
           <Cards
             src={this.state.imagesArray}
           />
